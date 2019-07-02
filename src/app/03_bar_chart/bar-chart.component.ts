bc5f804224ff912fc993f04e744a5583eff68405
Paul Hawk <phawk@aviahealthinnovation.com>
2019-07-02 15:01:23 -0500

    work

--- src/app/03_bar_chart/bar-chart.component.ts
+++ src/app/03_bar_chart/bar-chart.component.ts
@@ -18,6 +18,10 @@ export class BarChartComponent implements OnInit {
     title = 'Bar Chart';
 
     private width: number;
+    private bar_width: number;
+    private bar_height: number = 30;
+    private bar_padding: number = 4;
+    median_height: number = 20;
     private height: number;
     private margin = {top: 20, right: 20, bottom: 30, left: 40};
 
@@ -26,8 +30,9 @@ export class BarChartComponent implements OnInit {
     private svg: any;
     private g: any;
 
-    total_times_scored = 10;
-
+    benchmark_median = 1;
+    our_score = 1;
+    benchmark_translate: number;
     scores = [
       {name: "None", score_count: 2, score_percent: .20},
       {name: "Rudimentary", score_count: 2, score_percent: .20},
@@ -39,52 +44,36 @@ export class BarChartComponent implements OnInit {
 
     ngOnInit() {
         this.initSvg();
-        this.initAxis();
-        this.drawAxis();
         this.drawBars();
     }
 
     private initSvg() {
         this.svg = d3.select('svg');
         this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+        this.bar_width = ( this.width / 4 ) - this.bar_padding;
+        this.benchmark_translate = ( this.width / 4 ) * this.benchmark_median;
         this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
         this.g = this.svg.append('g')
-            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-    }
-
-    private initAxis() {
-        this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
-        this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-        this.x.domain(this.scores.map((d) => d.name));
-        this.y.domain([0, d3Array.max(this.scores, (d) => d.score_percent)]);
-    }
-
-    private drawAxis() {
-        this.g.append('g')
-            .attr('class', 'axis axis--x')
-            .attr('transform', 'translate(0,' + this.height + ')')
-            .call(d3Axis.axisBottom(this.x));
-        // this.g.append('g')
-        //     .attr('class', 'axis axis--y')
-           // .call(d3Axis.axisLeft(this.y).ticks(10, '%'))
-            // .append('text')
-            // .attr('class', 'axis-title')
-            // .attr('transform', 'rotate(-90)')
-            // .attr('y', 6)
-            // .attr('dy', '0.71em')
-            // .attr('text-anchor', 'end')
-            //.text('Frequency');
+         //   .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
     }
 
     private drawBars() {
         this.g.selectAll('.bar')
             .data(this.scores)
-            .enter().append('rect')
-            .attr('class', 'bar')
-            .attr('x', (d) => this.x(d.name) )
-            .attr('y', (d) => this.y(d.score_percent) )
-            .attr('width', this.width / 4 - 10)
-            .attr('height', (d) => this.height - this.y(d.score_percent) );
+            .enter().append('rect') // '#98abc5', '#8a89a6'
+            .style('fill', (d, i) => this.our_score  > i ? '#0076BC' : '#ffffff') // f8f8f8
+            .attr('x', (d, i) => i * (this.bar_width + this.bar_padding) )
+            .attr('y', 0)
+            .attr('width', this.bar_width)
+            .attr('height', this.bar_height );
+
+        this.svg.append('rect')
+            .attr("transform", `translate(${this.benchmark_translate},0) rotate(45)`)
+            .attr('x',  0)
+            .attr('y', 0)
+            .attr('width', this.median_height)
+            .attr('height', this.median_height)
+            .attr('fill', '#56AADB');
     }
 
 }
