85b508c6e7238384018c6c7d0953ea358546fceb
Paul Hawk <phawk@aviahealthinnovation.com>
2020-02-07 17:10:54 -0600

    work it baby

--- src/app/07_donut_chart/donut-chart.component.ts
+++ src/app/07_donut_chart/donut-chart.component.ts
@@ -15,19 +15,17 @@ export class DonutChartComponent implements OnInit {
 
     title = 'Donut Chart';
 
-    private width: number = 500;
-    private height: number = 250;
+    private width: number = 200;
+    private height: number = this.width / 2;
 
     private svg: any;     // TODO replace all `any` by the right type
 
     private radius: number;
 
-    private arc: any;
     private pie: any;
-    private color: any;
-
-    private g: any;
-
+    private benchmark_arc;
+    private score_arc;
+    private background_color = '#F8F8F8'
     pulse_score_list = {
       0: '#D8F1FF',
       1: '#A0DCFF',
@@ -45,65 +43,70 @@ export class DonutChartComponent implements OnInit {
       5: '#426415',
     };
 
-    benchmark_arc;
-    score_arc;
+
     constructor() {}
 
     ngOnInit() {
       const score_color_array = Object.keys(this.pulse_score_list).map(x => this.pulse_score_list[x]);
       const score_colors = d3Scale.scaleOrdinal()
         .range(score_color_array);
-      const bench_color_array = Object.keys(this.pulse_benchmark_list).map(x => this.pulse_benchmark_list[x]);
 
+      const bench_color_array = Object.keys(this.pulse_benchmark_list).map(x => this.pulse_benchmark_list[x]);
       const benchmark_colors = d3Scale.scaleOrdinal()
       .range(bench_color_array);
 
       this.initSvg();
 
-      this.drawChart(bench_color_array, benchmark_colors, this.benchmark_arc);
-      this.drawChart(score_color_array, score_colors, this.score_arc);
+      this.drawChart([1,2,3,4,5,6], benchmark_colors, this.benchmark_arc, 2);
+      this.drawChart([1,2,3,4,5,6], score_colors, this.score_arc, 3);
     }
 
     private initSvg() {
-        this.svg = d3.select('svg');
+      this.svg = d3.select('svg');
+      this.radius = this.height;
 
-        // this.width = +this.svg.attr('width');
-        // this.height = +this.svg.attr('height');
-        this.radius = this.height;
+      const scale = 450;
+      const margin = 10 * this.radius / scale;
+      const bench = 60 * this.radius / scale;
+      const score = 100 * this.radius / scale;
 
-        const anglesRange = 0.5 * Math.PI;
-        this.arc = d3Shape.arc()
-          .outerRadius(this.radius - 10)
-          .innerRadius(this.radius - 70)
-        this.benchmark_arc = d3Shape.arc()
-          .outerRadius(this.radius - 80)
-          .innerRadius(this.radius - 100)
+      const score_outer_arc = this.radius;
+      const score_inner_arc = score_outer_arc - score;
+      const bench_outer_arc = score_inner_arc - margin;
+      const bench_inner_arc = bench_outer_arc - bench;
 
-        this.score_arc = d3Shape.arc()
-          .outerRadius(this.radius - 10)
-          .innerRadius(this.radius - 70)
+      const anglesRange = 0.5 * Math.PI;
 
-        this.pie = d3Shape.pie()
-            .sort(null)
-            .value((d: any) => 1)
-            .startAngle( anglesRange * -1)
-            .endAngle( anglesRange)
-            .padAngle(.02)
+      this.score_arc = d3Shape.arc()
+        .outerRadius(score_outer_arc)
+        .innerRadius(score_inner_arc);
 
-        this.svg = d3.select('svg')
-            .append('g')
-            .attr('transform', 'translate(' + this.width / 2 + ',' + this.height + ')');
+      this.benchmark_arc = d3Shape.arc()
+        .outerRadius(bench_outer_arc)
+        .innerRadius(bench_inner_arc);
+
+      this.pie = d3Shape.pie()
+        .sort(null)
+        .value((d: any) => 1)
+        .startAngle( anglesRange * -1)
+        .endAngle( anglesRange)
+        .padAngle(.02);
+
+      this.svg = d3.select('svg')
+        .append('g')
+        .attr('transform', 'translate(' + this.width / 2 + ',' + this.height + ')');
     }
 
-    private drawChart(data, colors, arc) {
-        let g = this.svg.selectAll('.benchmark')
-            .data(this.pie(data))
-            .enter().append('g')
-            // .attr('class', 'arc');
+    private drawChart(data, colors, arc, score) {
+      const g = this.svg.selectAll('.arcs')
+        .data(this.pie(data))
+        .enter().append('g')
 
-        g.append('path')
-            .attr('d', arc)
-            .style('fill', (d, i) => colors(i) );
+      g.append('path')
+        .attr('d', arc)
+        .style('fill', (d, i) => (i <= score)
+          ? colors(i)
+          : this.background_color);
     }
 
 }
